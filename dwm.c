4b37b34a9bedc28f3fc76451a7a89e4857990d6c
jorgeloopzz <jorgeloopzz@protonmail.com>
2025-07-27 11:15:59 +0100

    Change path to look for the 'autostart' script

--- dwm.c
+++ dwm.c
@@ -1392,8 +1392,7 @@ run(void)
 
 void
 runAutostart(void) {
-	system("cd ~/.dwm; ./autostart_blocking.sh");
-	system("cd ~/.dwm; ./autostart.sh &");
+	system("cd ~/.config/dwm; ./autostart.sh &");
 }
 
 void
